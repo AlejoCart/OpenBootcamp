7218b6acbdbd7d188d53ed86a052aefb7ffb9104
AlejoCart <alejocartolano54@gmail.com>
2022-12-20 20:15:19 -0300

    first commit

---
+++ Java/PrimerTareaOB/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1=15;
+        System.out.println(num1);
+
+        double num2=15.5;
+        System.out.println(num2);
+
+        long num3= 144445L;
+        System.out.println(num3);
+
+        float num4=15.4564486F;
+        System.out.println(num4);
+
+        String PrimerString="Hola a todos, buenos dias/tardes/noches";
+        System.out.println(PrimerString);
+
+        boolean control=true;
+        System.out.println(control);
+
+        //System.out.println("Hello world!");
+    }
+}
