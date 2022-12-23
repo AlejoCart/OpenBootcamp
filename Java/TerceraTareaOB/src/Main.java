161638388334b25deb62405391f25c3222d78f2d
AlejoCart <alejocartolano54@gmail.com>
2022-12-22 21:24:59 -0300

    Tercer Ejecicio

---
+++ Java/TerceraTareaOB/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        String[] listaNombres={"Pepito","Juan Alberto","Miguel","Enrique"};
+
+        for(int i=0;i<4;i++){
+            System.out.println(listaNombres[i]);
+        }
+    }
+}
