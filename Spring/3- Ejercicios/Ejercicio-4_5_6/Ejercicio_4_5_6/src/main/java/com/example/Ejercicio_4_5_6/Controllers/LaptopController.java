00aa5d8c919e133a3c3c8549b3d84c884c19a95c
AlejoCart <alejocartolano54@gmail.com>
2023-03-22 13:03:52 -0300

    Ejercicios In progress

--- Spring/3- Ejercicios/Ejercicio-4_5_6/Ejercicio_4_5_6/src/main/java/com/example/Ejercicio_4_5_6/Controllers/LaptopController.java
+++ Spring/3- Ejercicios/Ejercicio-4_5_6/Ejercicio_4_5_6/src/main/java/com/example/Ejercicio_4_5_6/Controllers/LaptopController.java
@@ -3,10 +3,10 @@ package com.example.Ejercicio_4_5_6.Controllers;
 import com.example.Ejercicio_4_5_6.Entities.Laptop;
 import com.example.Ejercicio_4_5_6.Repositories.LaptopRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,15 +16,56 @@ import java.util.Optional;
 public class LaptopController {
 
     private LaptopRepository repositorio;
+    private final Logger log= LoggerFactory.getLogger(LaptopController.class);
     //@GetMapping(value={"api/Laptops","api/laptops"})
     @GetMapping(value = {"api/Laptops","api/Laptops/"})
     public List<Laptop> findAll(){
         return repositorio.findAll();
     }
 
+    @GetMapping("api/Laptop/{id}")
+    public ResponseEntity<Laptop> findOneById(@PathVariable Long id){
+        Optional<Laptop> aux=repositorio.findById(id);
+        return aux.isPresent() ? ResponseEntity.ok().build() :
+                ResponseEntity.notFound().build();
+    }
+
     @PostMapping("/api/Laptops/")
-    public Laptop create(@RequestBody Laptop lap){
-        return repositorio.save(lap);
+    public ResponseEntity<Laptop> create(@RequestBody Laptop lap){
+
+        if (lap.getId() == null) {
+            repositorio.save(lap);
+            return ResponseEntity.ok().build();
+        } else {
+            log.warn("Creating object with a ID diferent that null");
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/api/Laptop/update")
+    public ResponseEntity<Laptop> update(@RequestBody Laptop lap){
+
+        if(repositorio.existsById(lap.getId())){
+            repositorio.save(lap);
+            return ResponseEntity.ok().build();
+        }else {
+            log.warn("Updating object not found by ID");
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/api/Laptop/delete/{id}")
+    public ResponseEntity<Laptop> deleteById(@PathVariable Long id){
+
+
+        return null;
+    }
+
+    @DeleteMapping("/apí/Laptop/deleteAll")
+    public ResponseEntity<Laptop> deleteAll(){
+
+
+        return null;
     }
 
 }
